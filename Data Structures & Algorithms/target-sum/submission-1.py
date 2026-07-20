31007ffa9ce17912db5218c768b50b0190cd1960
hryliu <103466481+hryliu@users.noreply.github.com>
2026-07-19 21:56:36 -0400

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = {}
+        count = 0
+        inc = True
+
+        def dfs(i, inc, total):
+            nonlocal count
+
+            if inc:
+                curr_sum = total + nums[i]
+            else:
+                curr_sum = total - nums[i]
+
+            if i == len(nums) - 1: 
+                count += 1 if curr_sum == target else 0
+                return
+
+            dfs(i + 1, inc, curr_sum)
+            dfs(i + 1, not inc, curr_sum)
+
+        dfs(0, inc, 0)
+        dfs(0, not inc, 0)
+        return count
